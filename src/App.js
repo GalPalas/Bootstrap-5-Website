ed3c9ad21af3a723cd28822d204be89df20fe429
Gal <galpalas89@gmail.com>
2021-06-20 13:42:33 +0300

    create showCase area

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import "./App.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
+import WelcomePage from "./components/pages/welcomePage";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <WelcomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
